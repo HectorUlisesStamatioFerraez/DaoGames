77dccaf92964d81f4caf283bc598b01440f771b4
HectorUlisesStamatioFerraez <84391380+HectorUlisesStamatioFerraez@users.noreply.github.com>
2021-08-06 14:15:07 -0500

    Update DaoGames.java

--- src/main/java/mx/edu/utez/model/games/DaoGames.java
+++ src/main/java/mx/edu/utez/model/games/DaoGames.java
@@ -1,6 +1,5 @@
 package mx.edu.utez.model.games;
 
-import jdk.nashorn.internal.runtime.Context;
 import mx.edu.utez.model.category.BeanCategory;
 import mx.edu.utez.service.ConnectionMySQL;
 import org.slf4j.Logger;
@@ -14,11 +13,12 @@ import java.util.Base64;
 import java.util.List;
 
 public class DaoGames {
+    //*Practica DAOGames*//
     private Connection con;
     private CallableStatement cstm;
     private ResultSet rs;
-    private Logger CONSOLE = LoggerFactory.getLogger(DaoGames.class);
-
+    final private Logger CONSOLE = LoggerFactory.getLogger(DaoGames.class);
+    //*  Consulta general: findAll() *//
     public List<BeanGames> findAll(){
         List<BeanGames> listGames = new ArrayList<>();
         try {
@@ -32,97 +32,102 @@ public class DaoGames {
                 beanCategory.setName(rs.getString("name"));
                 beanCategory.setStatus(rs.getInt("status"));
                 beanGames.setIdGames(rs.getInt("idGames"));
-                beanGames.setName(rs.getString("name"));
+                beanGames.setName(rs.getString("Name"));
                 beanGames.setImgGame(Base64.getEncoder().encodeToString(rs.getBytes("imgGames")));
-                beanGames.setDatePremiere(rs.getString("date_premiere"));
+                beanGames.setDatePremiere(rs.getString("datePremiere"));
                 beanGames.setCategory_idCategory(beanCategory);
                 beanGames.setStatus(rs.getInt("status"));
                 listGames.add(beanGames);
             }
         }catch (SQLException e){
-            System.out.println("Ha ocurrido un error: " + e.getMessage());
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
         } finally {
             ConnectionMySQL.closeConnections(con, cstm, rs);
         }
         return listGames;
     }
-
-    public BeanGames findById(int id){
+    //* Consulta específica: findById() *//
+    public BeanGames findById(long id){
         BeanGames games = null;
         try {
             con = ConnectionMySQL.getConnection();
-            cstm = con.prepareCall("SELECT * FROM games WHERE idGames = ?;");
-            cstm.setInt(1, id);
+            cstm = con.prepareCall("SELECT * FROM videogames WHERE id = ? ");
+            cstm.setInt(1, (int) id);
             rs = cstm.executeQuery();
             if(rs.next()){
-                BeanCategory category = new BeanCategory();
+                BeanCategory beanCategory = new BeanCategory();
+                BeanGames beanGames = new BeanGames();
                 games = new BeanGames();
-                category.setIdCategory(rs.getInt("idCategory"));
-                category.setName(rs.getString("name"));
-                category.setStatus(rs.getInt("status"));
-                games.setIdGames(rs.getInt("idGames"));
-                games.setName(rs.getString("name"));
-                games.setImgGame(Base64.getEncoder().encodeToString(rs.getBytes("imgGames")));
-                games.setDatePremiere(rs.getString("date_premiere"));
-                games.setCategory_idCategory(category);
-                games.setStatus(rs.getInt("status"));
-                games.setCategory_idCategory(category);
+                beanCategory.setIdCategory(rs.getInt("idCategory"));
+                beanCategory.setName(rs.getString("name"));
+                beanCategory.setStatus(rs.getInt("status"));
+                beanGames.setIdGames(rs.getInt("idGames"));
+                beanGames.setName(rs.getString("Name"));
+                beanGames.setImgGame(Base64.getEncoder().encodeToString(rs.getBytes("imgGames")));
+                beanGames.setDatePremiere(rs.getString("datePremiere"));
+                beanGames.setCategory_idCategory(beanCategory);
+                beanGames.setStatus(rs.getInt("status"));
             }
         }catch (SQLException e){
-            System.out.println("Ha ocurrido un error: " + e.getMessage());
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
         } finally {
             ConnectionMySQL.closeConnections(con, cstm, rs);
         }
         return games;
     }
-    public boolean create(BeanGames games){
+    //* Registrar: create() *//
+    public boolean create(BeanGames beanGames){
         boolean flag = false;
         try{
             con = ConnectionMySQL.getConnection();
             cstm = con.prepareCall("{call sp_create(?,?,?,?)}");
-            cstm.setString(1, games.getCategory_idCategory().getName());
-            cstm.setString(2, games.getName());
-            cstm.setString(3, games.getImgGame());
-            cstm.setString(4, games.getDatePremiere());
+            cstm.setString(1, beanGames.getName());
+            cstm.setString(2, beanGames.getImgGame());
+            cstm.setString(3, beanGames.getDatePremiere());
+            cstm.setInt(4, beanGames.getCategory_idCategory().getIdCategory());
             cstm.execute();
             flag = true;
         }catch(SQLException e){
-            System.out.println("Ha ocurrido un error: " + e.getMessage());
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
         } finally {
             ConnectionMySQL.closeConnections(con, cstm);
         }
         return flag;
     }
-    public boolean update(BeanGames games){
+    //* Actualizar: update() *//
+    public boolean update(BeanGames beanGames){
         boolean flag = false;
         try{
             con = ConnectionMySQL.getConnection();
-            cstm = con.prepareCall("{call sp_create(?,?,?,?)}");
-            cstm.setString(1, games.getCategory_idCategory().getName());
-            cstm.setString(2, games.getName());
-            cstm.setString(3, games.getImgGame());
-            cstm.setString(4, games.getDatePremiere());
+            cstm = con.prepareCall("{call sp_update(?,?,?,?,?,?,?)}");
+            cstm.setInt(1, beanGames.getIdGames());
+            cstm.setString(2, beanGames.getName());
+            cstm.setString(3, beanGames.getImgGame());
+            cstm.setString(4, beanGames.getDatePremiere());
+            cstm.setInt(5, beanGames.getCategory_idCategory().getIdCategory());
+            cstm.setInt(6, beanGames.getStatus());
             flag = cstm.execute();
         }catch(SQLException e){
-            System.out.println("Ha ocurrido un error: " + e.getMessage());
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
         }finally{
             ConnectionMySQL.closeConnections(con, cstm);
         }
         return flag;
     }
+    //* Eliminar (baja lógica): delete() *//
     public boolean delete(int idGames){
         boolean flag = false;
         try{
             con = ConnectionMySQL.getConnection();
             cstm = con.prepareCall("{call sp_delete2(?)}");
-            cstm.setLong(1, idGames);
+            cstm.setInt(1, (int) idGames);
             flag = cstm.execute();
         }catch(SQLException e){
-            System.out.println("Ha ocurrido un error: " + e.getMessage());
+            CONSOLE.error("Ha ocurrido un error: " + e.getMessage());
         }finally{
             ConnectionMySQL.closeConnections(con, cstm);
         }
         return flag;
     }
-
 }
+
